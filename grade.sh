c5a59eeb879b0b0532e3857f22499bfb5e33145f
YiyaoL <76981300+YiyaoL@users.noreply.github.com>
2022-11-28 16:57:49 -0800

    update

--- grade.sh
+++ grade.sh
@@ -1,27 +1,30 @@
 CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
-rm -rf student-submission 
-git clone $1 student-submission
-echo "cloned" 
 
-if [ -e student-submission/ListExamples.java ]; then
-    cp TestListExamples.java student-submission 
-    cd student-submission
-    echo "copied TestListExamples.java"
+rm -rf student-submission
+git clone $1 student-submission
+cp TestListExamples.java student-submission/
+
+if [[ -e student-submission/ListExamples.java ]]
+then
+    echo "File exists!"
 else
-    echo "file doesn't exist"
+    echo "File not found. Please try again."
     exit 1
 fi
+
+cd student-submission
 
 javac -cp $CPATH *.java 
 if [ $? -eq 0 ]; then
-    echo "Compiled."
+    echo "Compiled Successfully!"
 else
-    echo "Failed to compile."
-    echo $?
+    echo "Failed to compile. Please try again."
     exit 1
 fi
 
-echo "Running tests..."
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > testout.txt
+echo "Begin running tests."
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
 echo "Test output:"
 grep -i "test" testout.txt 
+cat testout.txt
